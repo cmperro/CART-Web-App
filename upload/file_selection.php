904b812febb3e792ab099c7f4ae0ed48694037c6
Perro <cmperro@gmail.com>
2010-02-15 16:51:10 -0500

    Added upload stuff

---
+++ upload/file_selection.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+
+
+<form enctype="multipart/form-data" action="upload.php" method="POST">
+Please choose a file: <input name="uploaded" type="file" /><br />
+<input type="submit" value="upload" />
+</form>
+
+</body>
+</html>
---
+++ upload/upload.php
@@ -0,0 +1,32 @@
+<?php
+$target = "uploaded_spreadsheets/";
+$target = $target . basename( $_FILES['uploaded']['name']);
+$ok=1;
+
+$uploaded_type = $_FILES['uploaded']['type'];
+
+if (!($uploaded_type=="application/vnd.ms-excel"))
+{
+	echo "You may only upload XLS files.<br>";
+	$ok=0;
+}
+
+if ($ok==0)
+{
+	echo "Sorry your file was not uploaded";
+}
+
+
+else{
+
+
+	if(move_uploaded_file($_FILES['uploaded']['tmp_name'], $target))
+	{
+		echo "The file ". basename( $_FILES['uploaded']['name']). " has been uploaded";
+	}
+	else
+	{
+		echo "Sorry, there was a problem uploading your file";
+	}
+}
+?>
