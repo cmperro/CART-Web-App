ec13ae525d5553e3372a129bb05d9f7653774d3d
Perro <cmperro@gmail.com>
2010-02-16 10:39:21 -0500

    upload now works

--- upload/upload.php
+++ upload/upload.php
@@ -18,11 +18,9 @@ if ($ok==0)
 
 
 else{
-
-
 	if(move_uploaded_file($_FILES['uploaded']['tmp_name'], $target))
 	{
-		echo "The file ". basename( $_FILES['uploaded']['name']). " has been uploaded";
+		echo "The file ". basename( $_FILES['uploaded']['name']). " has been uploaded <a href='".$target."'>view</a>";
 	}
 	else
 	{
