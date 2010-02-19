642f9cfead86617c2e2aaa3154b48ac759791876
Perro <cmperro@gmail.com>
2010-02-18 22:16:45 -0500

    Commented up upload.php

--- upload/upload.php
+++ upload/upload.php
@@ -1,11 +1,24 @@
 <?php
+/**************************************************************
+*This script takes the uploaded file and does work.           *
+*More specifically, it saves the spreadsheet in a local       *
+*directory, checks to make sure that the user didn't enter    *
+*absurd cuttoff ranges, and then proceeds to read from the    *
+*uploaded excel spreadsheet. The answer key is saved in an    *
+*array. The student answers are then parsed and placed into   *
+*a two dimensional array.                                     *
+**************************************************************/
+
+
 $target = "uploaded_spreadsheets/";
 $target = $target . basename( $_FILES['uploaded']['name']);
 $COG = $_REQUEST['cutoff_grade'];
 $COP = $_REQUEST['cutoff_prob'];
-
 $uploaded_type = $_FILES['uploaded']['type'];
 
+//Various Error check, i.e. if the document uploaded is indeed
+//a 2003 Excel Spreadsheet, and making sure the two cuttoffs
+//are in the acceptable range
 if (!($uploaded_type=="application/vnd.ms-excel"))
 {
 	echo "You may only upload XLS files.<br>";
@@ -43,19 +56,28 @@ else{
 
 require_once 'Classes/PHPExcel/IOFactory.php';
 
+//Set-up a reader to parse the recently uploaded Excel Spreadsheet
 $objReader = PHPExcel_IOFactory::createReader('Excel5');
 $objPHPExcel = $objReader->load($target);
-$val = ($objPHPExcel->getActiveSheet()->getCell('A1'));
-$temp1 = $val->getvalue();
+//$val = ($objPHPExcel->getActiveSheet()->getCell('A1'));
+//$temp1 = $val->getvalue();
 
+
+//Ascertain the last filled Row and Column. This dynamically figures out how many students took
+//the test and how many questions the test was. This way the user doesn't need to be asked these
+//particulars
 $highestRow = $objPHPExcel->getActiveSheet()->getHighestRow();
 $highestCol = $objPHPExcel->getActiveSheet()->getHighestColumn();
+//Convert String Column to Int
 $highestCol = PHPExcel_Cell::columnIndexFromString($highestCol);
-
+//Hard-coded position of what column student answers start. This is the same for every sheet
 $startColumn = 'H';
 $startColumn = PHPExcel_Cell::columnIndexFromString($startColumn);
 
 
+
+
+//Set up and populate the Answer Key Array.
 $answerKey = array();
 
 for($col = $startColumn - 1; $col < $highestCol; $col++)
@@ -64,13 +86,13 @@ for($col = $startColumn - 1; $col < $highestCol; $col++)
 }
 
 //print_r($answerKey);
+//echo "<br>";
 
-echo "<br>";
 
-
+//Set up and populate the Student Answer Array. Start row is hard-coded. The answers start here for
+//every spreadsheet.
 $startRow = 5;
 $studentAns = array(array());
-
 
 for($row = $startRow; $row < $highestRow + 1; $row++)
 {
@@ -88,6 +110,8 @@ for($row = $startRow; $row < $highestRow + 1; $row++)
 	//echo "<br>";
 	$studentAns[] = $ithStudent;
 }
+
+//Test Routine - To Be Deleted
 /*
 $arrhigh = $highestRow - $startRow;
 for($k = 1; $k < $arrhigh + 2; $k++)
