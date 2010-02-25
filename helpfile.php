383681f771fafa76ca7d028c6da2e9658ec64993
Deniz Ulema <deniz.ulema@gmail.com>
2010-02-25 15:32:48 -0500

    added helpfile, link to help page in protected area

---
+++ helpfile.php
@@ -0,0 +1,58 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Help</title>
+<meta name="Generator" content="TextPad 4.7.3" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<style type="text/css">
+.TPtext { color: #000000; background-color: #FFFFFF; }
+</style>
+</head>
+
+<body class="TPtext">
+<pre>
+Authors: Andrew Curtis, Christopher Perro, Cooper Reid, Deniz Ulema
+
+As of 2/25, 2:32 PM
+
+Whenever you pull, be sure to:
+        chmod 777 accounts.txt
+        chmod 777 uploaded_spreadsheets
+                (Yes, I mean the directory uploaded_spreadsheets)
+
+Also, make sure you have a directory called "saved_pngs" then
+        chmod 777 saved_pngs
+
+CART definition: classification and regression tree
+
+User Instructions
+-----------------
+Login Page
+        Type in name, password
+        If you do not have a login, click on "request access", enter your email
+                The administrator shall be notified and will give you further
+                instructions and/or login info
+
+Protected Area
+        File to upload
+                Click "Browse...", then select a valid Excel Spreadsheet
+        Cutoff Grade
+                Enter the desired amount of questions answered correctly
+                Do NOT enter a percentage, or a letter grade
+        Cutoff Probability
+                Enter the desired cutoff probability
+                Enter a percentage, a number bewtween 0 and 1 (i.e. 0.3)
+        Click "upload"
+
+Results
+        If all input data was valid, the data entered is displayed here
+        The uploaded file may be viewed by clicking "view"
+        Click "Graph It!" to view the actual CART
+                Click on the image to view it on its own page
+                Right-click the image, then click "Save Image As..." to save
+        Click "Input new information" to return to the protected area, where
+        you can upload a new file and/or change cutoff values
+</pre>
+<a href="protected.php">Return to Protected Area</a>
+</body>
+</html>
--- protected.php
+++ protected.php
@@ -64,5 +64,8 @@ else {
 </div><!--close holder-->
 </div><!--close content-->
 </div><!--close wrapper-->
+
+<small><i>See the <span class="url-link"><a href="helpfile.php">README</a></span> for an explanation of how to use this application.</i></small>
+
 </body>
 </html>
