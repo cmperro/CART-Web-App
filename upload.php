afb953687e02e89a7f240a960a8951fadf34e64d
Deniz Ulema <deniz.ulema@gmail.com>
2010-02-25 10:50:14 -0500

    fixed error things temporarily

--- upload.php
+++ upload.php
@@ -75,14 +75,14 @@ $startColumn = PHPExcel_Cell::columnIndexFromString($startColumn);
 
 //numQuestions = (&highestCol - 7);//number of questions
 //Error check if the input cutoff values are allowable
-if ($COG < 0 || $COG > ($highestCol - 7) || strlen($COG) < 1 || !(is_numeric($element)))
+if ($COG < 0 || $COG > ($highestCol - 7) || strlen($COG) < 1)
 {
         echo "Please enter a valid cutoff grade (NOT percentage, but the number), between 0 and ".($highestCol - 7).".<br>";
         echo "<a href='protected.php'>Please try again.</a>";
         exit(1);
 }
 
-if ($COP < 0 || $COP > 1 || strlen($COP) < 1 || !(is_numeric($element)))
+if ($COP < 0 || $COP > 1 || strlen($COP) < 1)
 {
         echo "Please enter a valid cutoff probability, between 0 and 1.<br>";
         echo "<a href='protected.php'>Please try again.</a>";
