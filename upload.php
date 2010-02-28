ee5023f01d9d126cb4b5a8dfc1ed7380785fabef
Andrew Curtis <darkwaltz7@aol.com>
2010-02-28 15:31:26 -0500

    More comment changes

--- upload.php
+++ upload.php
@@ -6,7 +6,7 @@ session_start();
 *It also checks to make sure the user did not enter absurd cutoff ranges.
 *From there, it proceeds to read from the uploaded excel spreadsheet.
 *The answer key is saved in an array. The student answers are then parsed into a 2D-array.
-*</p>
+*<p>
 *
 * @package pagelevel-pagedesc
 */
