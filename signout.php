7b417c7a2c90d4b8140a29b80de33b561aa02d32
Cooper Reid <creid.student@manhattan.edu>
2010-02-28 21:15:49 -0500

    modified signout.php suppressed warning messages in the case that no files exist.

--- signout.php
+++ signout.php
@@ -4,20 +4,18 @@ session_start();
 * Signs out user at the end of their session
 */
 error_reporting(E_ALL);
-//clear session variable
-unset($_SESSION['loggedInAs']);
 
-$hmm = $_SESSION['filename'];
-
+@$hmm = $_SESSION['filename'];
 $mySpreadsheet = "uploaded_spreadsheets/$hmm";
 $myDOT = "saved_pngs/process.dot";
 $myPNG = "saved_pngs/output.png";
 
+//delete files
+@unlink($mySpreadsheet);
+@unlink($myDOT);
+@unlink($myPNG);
 
-unlink($mySpreadsheet);
-unlink($myDOT);
-unlink($myPNG);
-
+//clear session variable
 unset($_SESSION['loggedInAs']);
 unset($_SESSION['filename']);
 
